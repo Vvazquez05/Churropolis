decdc4b8f490c694ea2d7cb4497b45a936c50a7c
vvazquez <vdaniel.vazq@gmail.com>
2022-05-14 12:27:32 -0500

    Implementación de javascripts v1.1

--- js/main.js
+++ js/main.js
@@ -9,10 +9,18 @@ const Churro_relleno = new producto ("CHURRO RELLENO", 25);
 const Bites_churros = new producto ("BITES DE CHURROS", 35);
 const Malteada = new producto ("MALTEADA", 45);
 const Frappe = new producto ("FRAPPE", 55);
-const Cafe = new producto ("CAFÉ", 25);
+const Cafe = new producto ("CAFE", 25);
 
 const menu = [Churro_sencillo, Churro_relleno, Bites_churros, Malteada, Frappe, Cafe];
 console.log(menu);
+const mostrarMenu = () => {
+    let texto = "";
+    menu.forEach((eLement) => {
+        texto += `${eLement.nombre} $${eLement.precio}\n`
+    })
+    texto = `Menú:\n${texto}`
+    return texto;
+};
 let nombre = prompt ("¡Bienvenido a Churropolis! Por favor ingrese su nombre");
 while (nombre === ""){
     nombre = prompt ("¡Bienvenido a Churropolis! Por favor ingrese su nombre");
@@ -20,7 +28,7 @@ while (nombre === ""){
 let cantidad = parseInt (prompt((nombre.toUpperCase()) + ",  inserte la CANTIDAD de productos a elegir"));
 const comanda = [];
 for (let i = 1; i <= cantidad; i++){
-    let seleccion = prompt("Coloque el nombre del producto a comprar");
+    let seleccion = prompt(mostrarMenu()+"Coloque el nombre del producto a comprar ("+ i +"/"+cantidad+")");
     while (menu.some((el) => el.nombre == (seleccion.toUpperCase())) == false) {
         alert("No se ingresó correctamente");
         seleccion = prompt ("Coloque el nombre del producto a comprar");
@@ -39,4 +47,5 @@ const mostrarCompra = () => {
     })
     alert(`Los productos de compra son:\n${texto}\nEl monto total son $${suma}`);
 };
+
 mostrarCompra()
